76c646e2b4f91582be1a4a98659ada5ed098618d
Treadgold <laptoprecording@gmail.com>
2023-04-16 20:38:57 +1200

    fixed errors in audioboost.js

--- audioboost.js
+++ audioboost.js
@@ -5,7 +5,7 @@ function setGain(gainValue, thresholdValue, ratioValue) {
   if (!window.__ac) {
     window.__ac = new AudioContext();
   }
-  var comp = new DynamicsCompressorNode(window.__ac, { attack: 0.1 release: 0.5 ratio: ratioValue, threshold: thresholdValue });
+  var comp = new DynamicsCompressorNode(window.__ac, { knee: 10, attack: 0.01, release: 0.15, ratio: ratioValue, threshold: thresholdValue });
   var gain = new GainNode(window.__ac, { gain: gainValue });
   if (!window.__source) {
     var element = document.querySelector("video");
@@ -31,38 +31,46 @@ function toggleEnabled(newEnabled, { gainValue, thresholdValue, ratioValue }) {
   enabled = newEnabled;
 }
 
-browser.storage.local.get("enabled").then((result) => {
+// Load the stored gain value
+browser.storage.local.get(["gainValue", "thresholdValue", "ratioValue", "enabled"]).then((result) => {
+  const gainValue = result.gainValue || 10;
+  const thresholdValue = result.thresholdValue || -50;
+  const ratioValue = result.ratioValue || 10;
   enabled = result.enabled || false;
+
+  if (enabled) {
+      setGain(gainValue, thresholdValue, ratioValue);
+  }
 });
 
-function doit(e) {
-  if (enabled) {
-    browser.browserAction.setIcon({
-      path: {
-        48: "icons/audioboost-disabled.png",
-        96: "icons/audioboost-disabled.png"
-      }
-    });
-    browser.tabs.executeScript({
-      code: `
-        window.__source.disconnect();
-        window.__source.connect(window.__ac.destination);
-        undefined;
-      `
-    });
-  } else {
-    browser.browserAction.setIcon({
-      path: {
-        48: "icons/audioboost-enabled.png",
-        96: "icons/audioboost-enabled.png"
-      }
-    });
-    browser.storage.local.get(["gainValue", "thresholdValue", "ratioValue"]).then((result) => {
-      const gainValue = result.gainValue || 10;
-      const thresholdValue = result.thresholdValue || -50;
-      const ratioValue = result.ratioValue || 10;
-      setGain(gainValue, thresholdValue, ratioValue);
-    });
-  }
-  enabled = !enabled;
-}
+// function doit(e) {
+//   if (enabled) {
+//     browser.browserAction.setIcon({
+//       path: {
+//         48: "icons/audioboost-disabled.png",
+//         96: "icons/audioboost-disabled.png"
+//       }
+//     });
+//     browser.tabs.executeScript({
+//       code: `
+//         window.__source.disconnect();
+//         window.__source.connect(window.__ac.destination);
+//         undefined;
+//       `
+//     });
+//   } else {
+//     browser.browserAction.setIcon({
+//       path: {
+//         48: "icons/audioboost-enabled.png",
+//         96: "icons/audioboost-enabled.png"
+//       }
+//     });
+//     browser.storage.local.get(["gainValue", "thresholdValue", "ratioValue"]).then((result) => {
+//       const gainValue = result.gainValue || 10;
+//       const thresholdValue = result.thresholdValue || -50;
+//       const ratioValue = result.ratioValue || 10;
+//       setGain(gainValue, thresholdValue, ratioValue);
+//     });
+//   }
+//   enabled = !enabled;
+// }
