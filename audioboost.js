1820370e4065d5bce72716ec582541e3b2484101
Treadgold <laptoprecording@gmail.com>
2023-04-16 21:29:43 +1200

    remove unused code

--- audioboost.js
+++ audioboost.js
@@ -42,35 +42,3 @@ browser.storage.local.get(["gainValue", "thresholdValue", "ratioValue", "enabled
       setGain(gainValue, thresholdValue, ratioValue);
   }
 });
-
-// function doit(e) {
-//   if (enabled) {
-//     browser.browserAction.setIcon({
-//       path: {
-//         48: "icons/audioboost-disabled.png",
-//         96: "icons/audioboost-disabled.png"
-//       }
-//     });
-//     browser.tabs.executeScript({
-//       code: `
-//         window.__source.disconnect();
-//         window.__source.connect(window.__ac.destination);
-//         undefined;
-//       `
-//     });
-//   } else {
-//     browser.browserAction.setIcon({
-//       path: {
-//         48: "icons/audioboost-enabled.png",
-//         96: "icons/audioboost-enabled.png"
-//       }
-//     });
-//     browser.storage.local.get(["gainValue", "thresholdValue", "ratioValue"]).then((result) => {
-//       const gainValue = result.gainValue || 10;
-//       const thresholdValue = result.thresholdValue || -50;
-//       const ratioValue = result.ratioValue || 10;
-//       setGain(gainValue, thresholdValue, ratioValue);
-//     });
-//   }
-//   enabled = !enabled;
-// }
